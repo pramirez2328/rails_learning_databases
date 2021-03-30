f22f654924b38231472347df9626606d7f3046d8
Pedro Ramirez <prlara2000@gmail.com>
2021-03-29 20:25:36 -0500

    first commit, rails db

---
+++ db/migrate/20210329153550_create_users.rb
@@ -0,0 +1,11 @@
+class CreateUsers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :lastname
+      t.integer :age
+
+      t.timestamps
+    end
+  end
+end
