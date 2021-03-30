4297b98414eb67393e9af195773cc887a4d179ad
Pedro Ramirez <prlara2000@gmail.com>
2021-03-29 21:13:03 -0500

    adding show action

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   resources :articles
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :users, only: [:show]
+  get 'users/:id', to:'users#show'
 end
