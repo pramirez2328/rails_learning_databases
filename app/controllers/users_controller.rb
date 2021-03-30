445715c913d38b4231822904aaea7c1717622a1f
Pedro Ramirez <prlara2000@gmail.com>
2021-03-30 15:32:52 -0500

    adding the new and create actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,24 @@
 class UsersController < ApplicationController
 
   def index
-
+    @users = User.all
   end
+
   def show
     @user = User.find(params[:id])
   end
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params.require(:user).permit(:name, :lastname, :age))
+    if @user.save
+      flash[:notice] = "Great! a new user has been created."
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
- root "articles#index"
+ root "users#index"
   resources :articles
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-  get 'users/:id', to:'users#show'
-  get 'users', to:'users.index'
+  resources :users, only:[:index, :new, :create, :show]
 end
