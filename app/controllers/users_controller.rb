f86f86268c481ab2204976da50d7c427a2f45b4f
Pedro Ramirez <prlara2000@gmail.com>
2021-04-01 13:51:59 -0500

    refactoring

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,11 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   def index
     @users = User.all
   end
 
   def show
-    @user = User.find(params[:id])
   end
 
   def new
@@ -13,11 +13,11 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
+
   end
 
   def create
-    @user = User.new(params.require(:user).permit(:name, :lastname, :age))
+    @user = User.new(user_params)
     if @user.save
       flash[:notice] = "Great! a new user has been created."
       redirect_to @user
@@ -27,8 +27,7 @@ class UsersController < ApplicationController
   end
 
   def update
-    @user = User.find(params[:id])
-    if @user.update(params.require(:user).permit(:name, :lastname, :age))
+    if @user.update(user_params)
       flash[:notice] = "User was updated successfully."
       redirect_to @user
     else
@@ -37,8 +36,16 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    @user = User.find(params[:id])
     @user.destroy
     redirect_to users_path
+  end
+
+  private
+  def set_user
+        @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :lastname, :age)
   end
 end
